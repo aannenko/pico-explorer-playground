7f5f9d64be514e8cf99d57fad2c8ff7c406a0459
Andrii Annenko <roxton.public@gmail.com>
2025-11-15 22:05:03 +0100

    Improve wifi connection logic further

--- src/utilities/wifi.py
+++ src/utilities/wifi.py
@@ -10,8 +10,8 @@ from network import (
     STAT_CONNECT_FAIL,
 )
 
-ATTEMPT_TIMEOUT_MS = const(10_000)
-CONNECTING_SLEEP_MS = const(200)
+ATTEMPT_TTL_MS = const(10_000)
+CONNECTING_SLEEP_MS = const(100)
 DISCONNECTED_SLEEP_MS = const(1_000)
 
 
@@ -19,16 +19,20 @@ def try_connect(ssid: str, password: str, timeout_ms: int = 300_000) -> bool:
     if not ssid:
         return False
 
-    if timeout_ms < ATTEMPT_TIMEOUT_MS:
-        timeout_ms = ATTEMPT_TIMEOUT_MS
+    if timeout_ms < ATTEMPT_TTL_MS:
+        timeout_ms = ATTEMPT_TTL_MS
 
     wlan = WLAN(STA_IF)
     if not wlan.active():
         wlan.active(True)
 
+    if wlan.isconnected() and wlan.config("ssid") != ssid:
+        wlan.disconnect()
+        machine.idle()
+
     now_ms = time.ticks_ms()
     deadline_ms = time.ticks_add(now_ms, timeout_ms)
-    attempt_start_ms = time.ticks_add(now_ms, -ATTEMPT_TIMEOUT_MS)
+    attempt_start_ms = time.ticks_add(now_ms, -ATTEMPT_TTL_MS)
 
     while True:
         status = wlan.status()
@@ -36,6 +40,7 @@ def try_connect(ssid: str, password: str, timeout_ms: int = 300_000) -> bool:
             return True  # connected
 
         if status == STAT_WRONG_PASSWORD:
+            wlan.disconnect()
             return False  # wrong password
 
         now_ms = time.ticks_ms()
@@ -44,12 +49,12 @@ def try_connect(ssid: str, password: str, timeout_ms: int = 300_000) -> bool:
             return False  # timeout
 
         if status == STAT_CONNECTING:
-            if time.ticks_diff(now_ms, attempt_start_ms) >= ATTEMPT_TIMEOUT_MS:
-                wlan.disconnect()
-                time.sleep_ms(DISCONNECTED_SLEEP_MS)
-            else:
+            if time.ticks_diff(now_ms, attempt_start_ms) < ATTEMPT_TTL_MS:
                 time.sleep_ms(CONNECTING_SLEEP_MS)
                 continue
+            else:  # took too long to connect
+                wlan.disconnect()
+                time.sleep_ms(DISCONNECTED_SLEEP_MS)
         elif status == STAT_NO_AP_FOUND or status == STAT_CONNECT_FAIL:
             if remaining_ms > DISCONNECTED_SLEEP_MS:
                 time.sleep_ms(DISCONNECTED_SLEEP_MS)
